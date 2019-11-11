9c9f5c97240ff744713e4952478b2ddbcf9828e2
Tony Waterfield <tonywaterfieldrva@gmail.com>
2019-11-10 21:24:32 -0500

    updated logic on javascript

--- appFolder/routing/htmlRoutes.js
+++ appFolder/routing/htmlRoutes.js
@@ -19,7 +19,7 @@ app.get("/survey", function(req, res) {
     //  console.log("inside get /home");
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
-// catch all if /home or /survey not requested
+//  catch all if /home or /survey not requested
   app.get("/", function(req, res) {
     //  console.log("inside get /home");
     res.sendFile(path.join(__dirname, "../public/home.html"));
